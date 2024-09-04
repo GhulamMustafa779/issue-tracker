9908ed70dabb2dbc67ff82cd8868038c48bb7f4a
GhulamMustafa779 <mustafayousaf779@gmail.com>
2024-09-04 21:24:14 +0500

    Refactor: Build Edit issue page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,50 +1,39 @@
-import React from 'react'
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation'
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
-import ReactMarkdown from 'react-markdown'
-import delay from 'delay';
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import Link from 'next/link'
+import React from "react";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import { Box, Grid } from "@radix-ui/themes";
+import delay from "delay";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
-interface Props{
-    params: {id: string}
+
+interface Props {
+  params: { id: string };
 }
 
-const IssueDetailsPage = async ({params}: Props) => {
-    //if (typeof params.id !== 'number') notFound();
+const IssueDetailsPage = async ({ params }: Props) => {
+  //if (typeof params.id !== 'number') notFound();
 
-    const issue = await prisma.issue.findUnique({
-        where: {
-            id: parseInt(params.id)
-        }
-    })
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
 
-    if(!issue) notFound();
+  if (!issue) notFound();
 
-    await delay(2000);
+  await delay(2000);
 
   return (
-    <Grid columns={{initial:"1", md: "2"}} gap="5">  
-        <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex gap="5" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className='prose' mt="5">
-            <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
-        </Box> 
-        <Box>
-            <Button>
-                <Pencil2Icon />
-                <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-            </Button>
-        </Box>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <IssueDetails issue={issue} />
+      </Box>
+      <Box>
+        <EditIssueButton issueId={issue.id} />
+      </Box>
     </Grid>
-  )
-}
+  );
+};
 
-export default IssueDetailsPage
+export default IssueDetailsPage;
---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import React, { useState } from "react";
+import { TextField, Button, Callout} from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
+import dynamic from "next/dynamic";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/misc/validationSchemas";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import { Issue } from "@prisma/client";
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+
+const IssueForm = ({issue}: {issue?:Issue}) => {
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState<boolean>(false);
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  });
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false); 
+      setError("An unexpected error has occurred");
+    }
+  })
+
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={onSubmit}
+      >
+        <TextField.Root defaultValue={issue?.title} placeholder="Title" {...register("title")} />
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          defaultValue={issue?.description}
+          render={({ field }) => {
+            const { ref, ...rest } = field; // removes ref
+            return <SimpleMDE placeholder="Enter the description" {...rest} />;
+          }}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button type="submit" disabled={isSubmitting}>
+          Create New Issue {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default IssueForm;
