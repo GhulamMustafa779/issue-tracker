7c551ea4a5e1bd3c8ba6e3edf49240e159de612f
GhulamMustafa779 <mustafayousaf779@gmail.com>
2024-09-04 15:54:46 +0500

    add loading pages and markdown preview

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import prisma from '@/prisma/client'
+import { notFound } from 'next/navigation'
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import ReactMarkdown from 'react-markdown'
+
+interface Props{
+    params: {id: string}
+}
+
+const IssueDetailsPage = async ({params}: Props) => {
+    //if (typeof params.id !== 'number') notFound();
+
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: parseInt(params.id)
+        }
+    })
+
+    if(!issue) notFound();
+
+  return (
+    <div>  
+        <Heading>{issue.title}</Heading>
+        <Flex gap="5" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className='prose' mt="5">
+            <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+    </div>
+  )
+}
+
+export default IssueDetailsPage
