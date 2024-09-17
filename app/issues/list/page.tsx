2c0e441869288be7f83c0dd85ffb42e55512d50a
GhulamMustafa779 <mustafayousaf779@gmail.com>
2024-09-17 14:14:48 +0500

    make column sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,24 +1,33 @@
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
 // import Link from '@/app/components/Link'
 // import IssueStatusBadge from "../components/IssueStatusBadge";
-import { Link, IssueStatusBadge} from '@/app/components' // making an index file in components folder makes import easy and clean
+import { Link, IssueStatusBadge } from "@/app/components"; // making an index file in components folder makes import easy and clean
 import prisma from "@/prisma/client";
-import delay from 'delay'
+import delay from "delay";
 import IssueActions from "./IssueActions";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import NextLink from 'next/link'
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
-  searchParams: {status: Status}
+  searchParams: { status: Status, orderBy: keyof Issue};
 }
 
-const IssuesPage = async ({searchParams}:Props) => {
-  const statuses = Object.values(Status)
-  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
+const IssuesPage = async ({ searchParams }: Props) => {
+  const columns: { label: string; value: keyof Issue; className?: string }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
   const issues = await prisma.issue.findMany({
-    where:{
-      status
-    }
+    where: {
+      status,
+    },
   });
   await delay(2000);
   return (
@@ -27,22 +36,27 @@ const IssuesPage = async ({searchParams}:Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {
+              columns.map((column)=>{
+                return (
+                  <Table.ColumnHeaderCell 
+                  key={column.value}
+                  className={column.className || ''}>
+                    <NextLink href={{
+                      query:{...searchParams, orderBy: column.value}
+                    }}>{column.label}</NextLink>
+                    {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+                  </Table.ColumnHeaderCell>
+                )
+              })
+            }
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </Link>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
@@ -62,6 +76,6 @@ const IssuesPage = async ({searchParams}:Props) => {
 };
 
 // export const dynamic = 'force-dynamic'
-export const revalidate = 0; 
+export const revalidate = 0;
 
 export default IssuesPage;
